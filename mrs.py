8863ecb89c2b84c30b357f5493b743d95c275841
Manvitha0 <86139814+Manvitha0@users.noreply.github.com>
2023-12-03 15:42:12 +0530

    Add files via upload

---
+++ mrs.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import streamlit as st
+import pickle
+import requests
+import streamlit.components.v1 as components
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=020b311fe0559698373a16008dc6a672&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+
+
+
+
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+
+similarity = pickle.load(open('simi.pkl', 'rb'))
+st.header('Movie Recommender System')
+imageCarouselComponent = components.declare_component("image-carousel-component", path="frontend/public")
+imageUrls = [
+    fetch_poster(1632),
+    fetch_poster(299536),
+    fetch_poster(17455),
+    fetch_poster(2830),
+    fetch_poster(429422),
+    fetch_poster(9722),
+    fetch_poster(13972),
+    fetch_poster(240),
+    fetch_poster(155),
+    fetch_poster(598),
+    fetch_poster(914),
+    fetch_poster(255709),
+    fetch_poster(572154)
+   
+    ]
+
+
+imageCarouselComponent(imageUrls=imageUrls, height=200)
+# selectvalue=st.selectbox("Select movie from dropdown", movies)
+selected_movie_name = st.selectbox(
+    'What movie would you like to search?',
+    movies['title'].values
+)
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_posters = []
+    for x in movies_list:
+        movie_id = movies.iloc[x[0]].movie_id
+        recommended_movies.append(movies.iloc[x[0]].title)
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
